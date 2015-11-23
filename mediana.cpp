806787b5e1085d71743b51e722e4ab813c2c28c2
tonyupmp <ferrari965_tony@hotmail.com>
2015-11-23 13:35:19 +0100

    evidencia
    fundamento de programación

---
+++ mediana.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int arreglo[10];
+float mitadArreglo;
+float longitud;
+int mediana,i;
+int main()
+{
+	for(i=0;i <10; i++){
+	printf("dame el numero\n");
+	scanf("%d",&arreglo[i]);
+}
+
+	{
+	for(i=0; i<10; 	i++){
+	longitud=10;
+	mitadArreglo=longitud/2;
+	}
+		for(i=0; i<10; 	i++){
+			mediana=(arreglo[4]+arreglo[5])/2;
+			
+}
+for(i=0; i>10; 	i++){
+	if(arreglo[4]<arreglo[5]/2);
+}
+
+}
+				printf("imprime el numero de la mediana %d\n",mediana+1);
+							return (0);
+}
+
+
